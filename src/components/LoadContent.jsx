36ed31fa1809ad28d8b6bd0e02e8aa264cd18ecc
denisavl <denisavlad604@gmail.com>
2023-12-06 11:02:47 +0200

    Add LoadContent function and Download Button

--- src/components/LoadContent.jsx
+++ src/components/LoadContent.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
-export default function CreateContent({onDelete, onLoad}){
+import downloadIcon from '../assets/download.svg'
+export default function CreateContent({onDelete, onLoad, onDownload}){
     return (
-        <div className="top-content form-container">
+        <div id="top-content">
             <button className="clearBtn" type="button" onClick={onDelete}>Clear Resume</button>
             <button className="loadBtn" type="button" onClick={onLoad}>Load Example</button>
+            <button className="downloadBtn" type='button' onClick={onDownload}>
+                <span>Download </span><img src={downloadIcon} alt="download icon" id='download-icon'/>
+            </button>
         </div>
     )
 }
